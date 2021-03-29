9071c1e3ed8be059615ab5bdc98228060b7828c5
Pierre Ducher <pierre.ducher@365talents.com>
2021-03-29 16:52:50 +0200

    Upgrade plugin for hapi 17 and above

--- test/index.js
+++ test/index.js
@@ -1,130 +1,100 @@
 'use strict';
 
-var Code = require('code');
-var Hapi = require('hapi');
-var Lab = require('lab');
-var ElasticSearch = require('elasticsearch');
-var ElasticSearchPlugin = require('../lib/index');
+const Code = require('@hapi/code');
+const Hapi = require('@hapi/hapi');
+const Lab = require('@hapi/lab');
+const ElasticSearch = require('elasticsearch');
+const ElasticSearchPlugin = require('../lib/index');
 
-var internals = {};
+const { it, beforeEach, describe } = exports.lab = Lab.script();
+const { expect } = Code;
 
-var lab = exports.lab = Lab.script();
-var beforeEach = lab.beforeEach;
-var afterEach = lab.afterEach;
-var describe = lab.describe;
-var it = lab.it;
-var expect = Code.expect;
+describe('Plugin client Tests', () => {
 
-describe('Plugin client Tests', function () {
+    it('should expose client instance by the `es` property', async () => {
 
-    it('should expose client instance by the `es` property', function (next) {
+        const server = new Hapi.Server();
+        await server.register({ plugin: ElasticSearchPlugin, options: {} });
 
-        var server = new Hapi.Server();
-        server.connection();
-        server.register({ register: ElasticSearchPlugin, options: {} }, function (err) {
-
-            if (err) { return next(err); }
-            var plugin = server.plugins['hapi-elastic'];
-            expect(plugin.es).to.exist();
-            next();
-        });
+        const plugin = server.plugins['hapi-elastic'];
+        expect(plugin.es).to.exist();
     });
 
-    it('should default configuration be localhost & port as 9200', function (next) {
+    it('should default configuration be localhost & port as 9200', async () => {
 
-        var server = new Hapi.Server();
-        server.connection();
-        server.register({ register: ElasticSearchPlugin, options: {} }, function (err) {
+        const server = new Hapi.Server();
+        await server.register({ plugin: ElasticSearchPlugin, options: {} });
 
-            if (err) { return next(err); }
-            var config = server.plugins['hapi-elastic'].es.transport._config;
-            expect(config.localhost).to.exist();
-            expect(config.localhost).to.equal(9200);
-            next();
-        });
+        const config = server.plugins['hapi-elastic'].es.transport._config;
+        expect(config.localhost).to.exist();
+        expect(config.localhost).to.equal(9200);
     });
 
-    it('should pass all `config` options hash to the client', function (next) {
+    it('should pass all `config` options hash to the client', async () => {
 
-        var server = new Hapi.Server();
-        server.connection();
-        server.register({ register: ElasticSearchPlugin, options: {
+        const server = new Hapi.Server();
+        await server.register({ plugin: ElasticSearchPlugin, options: {
             config: {
-                apiVersion: '1.2'
+                apiVersion: '1.7'
             }
-        } }, function (err) {
+        } });
 
-            if (err) { return next(err); }
-            var config = server.plugins['hapi-elastic'].es.transport._config;
-            expect(config.apiVersion).to.equal('1.2');
-            next();
-        });
+        const config = server.plugins['hapi-elastic'].es.transport._config;
+        expect(config.apiVersion).to.equal('1.7');
     });
 });
 
-describe('Test handling of Elastic Errors', function () {
+describe('Test handling of Elastic Errors', () => {
 
-    var server;
-    beforeEach( function (next) {
+    let server;
+    beforeEach( async () => {
 
         server = new Hapi.Server();
-        server.connection();
-        server.register({ register: ElasticSearchPlugin, options: {} }, function (err) {
+        await server.register({ plugin: ElasticSearchPlugin, options: {} });
 
-            if (err) { return next(err); }
+        //add routes
 
-            //add routes
+        //success route
+        server.route({ method: 'GET', path: '/query',
+            handler() {
 
-            //success route
-            server.route({ method: 'GET', path: '/query',
-                handler: function (request, reply) {
+                return { success: true };
+            }
+        });
 
-                    reply({ success: true });
-                }
-            });
+        //create an error route
+        const ErrRoute = function (path, Error) {
 
-            //create an error route
-            var ErrRoute = function (path, Error) {
+            this.method = 'GET';
+            this.path = path;
+            this.handler = () => {
 
-                this.method = 'GET';
-                this.path = path;
-                this.handler = function (request, reply) {
-
-                    reply(new Error());
-                };
+                throw new Error();
             };
+        };
 
-            server.route(new ErrRoute('/not-found', ElasticSearch.errors.NotFound));
-            server.route(new ErrRoute('/conflict', ElasticSearch.errors.Conflict));
-            server.route(new ErrRoute('/generic', ElasticSearch.errors.Generic));
-            next();
-        });
+        server.route(new ErrRoute('/not-found', ElasticSearch.errors.NotFound));
+        server.route(new ErrRoute('/conflict', ElasticSearch.errors.Conflict));
+        server.route(new ErrRoute('/generic', ElasticSearch.errors.Generic));
     });
 
-    it('Test success call', function (next) {
+    it('Test success call', async () => {
 
-        server.inject('/query', function (resp) {
-
-            expect(resp.statusCode).to.equal(200);
-            next();
-        });
+        const resp = await server.inject('/query');
+        expect(resp.statusCode).to.equal(200);
     });
 
-    it('Not Found(404)', function (next) {
+    it('Not Found(404)', async () => {
 
-        server.inject('/not-found', function (resp) {
+        const resp =  await server.inject('/not-found');
 
-            expect(resp.statusCode).to.equal(404);
-            next();
-        });
+        expect(resp.statusCode).to.equal(404);
     });
 
-    it('Conflict(409)', function (next) {
+    it('Conflict(409)', async () => {
 
-        server.inject('/conflict', function (resp) {
+        const resp =  await server.inject('/conflict');
 
-            expect(resp.statusCode).to.equal(409);
-            next();
-        });
+        expect(resp.statusCode).to.equal(409);
     });
 });
